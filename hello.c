fb61c60db64c9d19cc1a6a963106ad14548716f2
MadJon12 <123793208+MadJon12@users.noreply.github.com>
2023-01-30 22:34:08 -0600

    added my major to the output

---
+++ hello.c
@@ -0,0 +1,16 @@
+/**
+ * Author: Madeline Jones
+ * Date: 20/01/30
+ *
+ * A simple Hello world program in C
+ *
+ */
+ #include <stdlib.h>
+ #include <stdio.h>
+
+ int main(int argc, char **argv){
+    printf("Hello World!\n");
+    printf("My name is Madeline Jones!\n");
+    printf("My major is Computer Engineering.\n");
+    return 0;
+ }
